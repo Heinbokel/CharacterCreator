f51beb71949ea073f9940c7f18a66a45c248b94d
Robert Heinbokel <robertheinbokel@gmail.com>
2024-04-04 11:39:28 -0400

    Finished

---
+++ Pages/CharacterCreation.razor.cs
@@ -0,0 +1,24 @@
+using CharacterCreator.Models;
+using CharacterCreator.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace CharacterCreator.Pages;
+
+public partial class CharacterCreation: ComponentBase {
+
+    // Inject our Character Service as we have seen elsewhere.
+    [Inject]
+    private CharacterService _characterService {get; set;}
+
+    // Holds our Character that our form is bound to.
+    private Character Character {get; set;} = new Character();
+
+    // Submits the form, creating the character and resetting our local character.
+    // The Character Service will take care of adding this character to its list 
+    // and notifying all observers that the list of characters has changed.
+    private void SubmitForm() {
+        this._characterService.AddCharacter(this.Character);
+        this.Character = new Character();
+    }
+
+}
